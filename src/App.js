a06270d966f1e3d7c773bfdb40ec5d46dc8fc937
barakaganai <barakaganai01@gmail.com>
2022-09-09 13:24:16 +0300

    import topBar component to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import TopBar from "./topBar/TopBar";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <div className="App">
+      <TopBar />
+    </div>
+  );
 }
 
 export default App;
