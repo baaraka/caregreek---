f302f935253578b713dd2bea536d65f3ab2b407d
barakaganai <barakaganai01@gmail.com>
2022-09-09 13:39:32 +0300

    import Home component into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./footer/Footer";
+import Home from "./home/Home";
 import TopBar from "./topBar/TopBar";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Home />
       <Footer />
     </div>
   );
