8bcdde52894a33c090a9f61c789f6339fb31e01d
barakaganai <barakaganai01@gmail.com>
2022-10-17 14:44:25 +0300

    import Contact.jsx component into App.jsx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Contact from "./contact/Contact";
 import Footer from "./footer/Footer";
 import Home from "./home/Home";
 import TopBar from "./topBar/TopBar";
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <TopBar />
       <Home />
+      <Contact />
       <Footer />
     </div>
   );
