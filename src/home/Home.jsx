ebae08a2ded99903577f940e177eb651a1f041a1
barakaganai <barakaganai01@gmail.com>
2022-12-07 10:54:18 +0300

    add div

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,25 +5,27 @@ export default function Home() {
     <div className="home">
       <div className="homeImg">
         <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN8AGVDIeUjrl5JwL3-QpUsdfRPgK5dO0m8OCVAxkaOkka9VrWptzD3k7e31WF8F3lJnQ&usqp=CAU"
+          src="https://img.freepik.com/premium-photo/woman-wearing-rubber-gloves-cleaning-kitchen-cabinets-surface-cleaning-concept_359031-20790.jpg?w=2000"
           alt=""
           className="homeImgItem"
         />
       </div>
-      <div className="homeTitle">
-        <h1 className="homeTitleItem">Contact Us</h1>
-      </div>
-      <div className="homeList">
-        <ul className="homeListItem">
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-        </ul>
+      <div className="homeTitleTitle">
+        <div className="homeTitle">
+          <h1 className="homeTitleItem">Contact Us</h1>
+        </div>
+        <div className="homeList">
+          <ul className="homeListItem">
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
